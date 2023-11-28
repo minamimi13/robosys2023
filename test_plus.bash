1d7490d1821a244b9fcd523fc9bf72f24cdf9142
minamimi <minamimimi>
2023-11-28 14:31:15 +0900

    Add a command

--- test_plus.bash
+++ test_plus.bash
@@ -1,6 +1,26 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $1
+      rest=1
+}
+
+res=0
+
+### I/O ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+  exit $res
