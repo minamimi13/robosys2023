ed9e513f9a838388f245e790eadc7b9f7f1d74d4
minamimi <minamimimi>
2023-11-29 10:09:06 +0900

    Edit test_Uranai

---
+++ test_Uranai.bash
@@ -0,0 +1,29 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Saori Kitami
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+      echo NG at Line $1
+      rest=1
+}
+
+res=0
+
+### I/O ###
+out=$( echo 誕生月を入力してください： | ./Uranai)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo 5 | ./plus)
+[ "${out}" = 5 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "エラー：数字ではありません" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "エラー：数字ではありません" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+  exit $res
